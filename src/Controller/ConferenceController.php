b14ab4f6b7b5e9ad6735e21ae82d8289d5db6b4c
jesusYanez <jesus.yanez@gopenux.com>
2024-02-21 17:57:56 -0500

    Creacion de controlador, obtener parametros de la peticion de tipo query param

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'app_conference')]
+    public function index(Request $request): Response
+    {
+        $greet = '';
+
+        if($name = $request->query->get('hello')){
+            $greet = sprintf('<h1>Hello %s! </h1>', htmlspecialchars($name));
+        }
+        //return $this->render('conference/index.html.twig');
+        return new  Response(<<<EOF
+<html>
+    <body>
+    $greet
+        <h1>Hola mundo desde Symfony</h1>
+    </body>
+</html> 
+EOF
+        );
+    }
+}
