c89c507eb2abd8feb10c55a890965acc7c4d66ae
jesusYanez <jesus.yanez@gopenux.com>
2024-02-22 08:25:53 -0500

    Parametro de tipo path

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
-    public function index(Request $request): Response
+    #[Route('/hello/{name}', name: 'app_conference')]
+    public function index(string $name ): Response
     {
         $greet = '';
 
-        if($name = $request->query->get('hello')){
+        if($name){
             $greet = sprintf('<h1>Hello %s! </h1>', htmlspecialchars($name));
         }
         //return $this->render('conference/index.html.twig');
